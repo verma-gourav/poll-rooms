56127cdbf8a0e1eefafdf641c04b5d184aef6bb1
Gourav Verma <gouravv.contact@gmail.com>
2026-04-26 13:37:26 +0000

    feat: add poll schema

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -4,4 +4,10 @@ export const CreateRoomSchema = z.object({
   title: z.string().min(3).max(30),
 });
 
+export const CreatePollSchema = z.object({
+  question: z.string().min(5),
+  options: z.array(z.string().min(1)).min(2).max(5),
+});
+
 export type CreateRoomInput = z.infer<typeof CreateRoomSchema>;
+export type CreatePollInput = z.infer<typeof CreatePollSchema>;
