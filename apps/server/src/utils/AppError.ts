425365f995b3b634eafbf9279c9c54adabb749a3
Gourav Verma <gouravv.contact@gmail.com>
2026-04-26 13:24:11 +0000

    feat: implement AppError

---
+++ apps/server/src/utils/AppError.ts
@@ -0,0 +1,12 @@
+export class AppError extends Error {
+  public statusCode: number;
+  public errors?: any;
+
+  constructor(message: string, statusCode: number, errors?: any) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    Object.setPrototypeOf(this, AppError.prototype);
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
